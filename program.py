241e6142d7e3df7c77d743e70f16f509e82d0bef
umarazam <umarazam01@gmail.com>
2019-05-16 17:38:19 +0500

    reshape and 1-d Conversion succesfull for one parameter --> Succesfull. Change it to work with whole list

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from math import sqrt
 from glob import glob
 from timeit import default_timer as timer
@@ -7,7 +8,7 @@ from helper import *
 def load_images(path):
 	try:
 		filenames = glob(path)
-		#filenames.sort()
+		filenames.sort()
 		images= [cv2.imread(img) for img in filenames]
 		return images
 	except Exception:
@@ -22,13 +23,12 @@ def featureExtraction(img):
 	sift = cv2.xfeatures2d.SIFT_create()
 	i=0
 	for im in img:
-		if i > 2:
+		if i> 3:
 			break
 		i+=1
 		kp,d = sift.detectAndCompute(im,None)
 		ds.append(d)
 		kps.append(kp)
-
 	return kps,ds
 
 def euclidean_Distance(x=1,y=1, x1=0, y1 =0):
@@ -37,13 +37,47 @@ def euclidean_Distance(x=1,y=1, x1=0, y1 =0):
 
 def knn(k, descriptors, n_des):
 	dis_list = []
+	length = len(n_des)
 	for a in descriptors:
+		length_1 = len(a)
 		pass
+
+def reshape(ds1, ds2):
+	# Reshape array function
+	flag = True
+	ds1 = ds1.flatten()
+	ds2 = ds2.flatten()
+	if len(ds2) > len(ds1):
+		flag = False
+	if flag:  #	It will reshape and 1-D ds2 will be of same shape ds1
+		dif = len(ds1)-len(ds2) # --> Correct
+		zeros = np.zeros((1, dif))
+		ds2= np.append(ds2, zeros)
+	else:
+		print('Here')
+		dif = len(ds2)-len(ds1) # --> Correct
+		zeros = np.zeros((1, dif))
+		ds1= np.append(ds1, zeros)
+		print(len(ds1))
+		print(len(ds2))
 
 def kp_matching(kp1,kp2):
 	if kp1 == kp2:
 		return True
 	return False
+
+def knn_intialize(data,des):
+	knn = cv2.ml.KNearest_create()
+	print(len(data))
+	print(len(des))
+	for im,ds in zip(data,des):
+		knn.train(im,ds)
+	return knn
+
+def knn_result(knn, k ,data):
+	ret,results,neighbours, dis = knn.find_nearest(data,k)
+	return ret, results, neightbours,dist
+
 
 def main():
 	forest_images_path = '/home/umar/dip_project/New_Try/Database/forest/*.jpg'
@@ -51,9 +85,10 @@ def main():
 
 	print("SIFT Working........")
 	kps, ds = featureExtraction(images)
-	print(kp_matching(kps[0],kps[1]))
+#	Classification
+
+	reshape(ds[1],ds[0]) # --> Reshaped correctly
 
 
 if __name__ == '__main__':
 	main()
-
