fc1ed18b8077fb2551c3a5dfd1efffe2e0a7a21d
umarazam <umarazam01@gmail.com>
2019-05-17 17:11:26 +0500

    Working--> Just add more images in train and resuffle in the test directory as well

--- program.py
+++ program.py
@@ -1,18 +1,16 @@
+#program.py
+
 import cv2
 import numpy as np
 from math import sqrt, floor
-from glob import glob
 from timeit import default_timer as timer
 from helper import *
 
-def load_images(path):
-	try:
-		filenames = glob(path)
-		filenames.sort()
-		images= [cv2.imread(img) for img in filenames]
-		return images
-	except Exception:
-		print(f"Couldn't Load the images with path {path}")
+# --> Names for pickle objects
+path_kp_train = "kp_train.pickle"
+path_kp_test = "kp_test.pickle"
+path_ds_train = "ds_train.pickle"
+path_ds_test = "ds_test.pickle"
 
 # SIFT Implementation
 def featureExtraction(img):
@@ -32,14 +30,14 @@ def knn(k, descriptors, n_des):
 		d_train, d_test = reshape_1D(a, n_des)
 		res=floor(sqrt(sum((d_train-d_test) **2)))
 		temp_d = {index:res}
-		print(f'index: {index}, Result: {res}')
+	#	print(f'index: {index}, Result: {res}')
 		if len(matches) < k:
 			matches.update(temp_d)
 		else:
 			for key,v in matches.items():
 				if v > res:
 					matches.pop(key,'Not Found')
-					print('Updated')
+					#print('Updated')
 					matches.update(temp_d)
 					break
 		del temp_d
@@ -64,37 +62,62 @@ def reshape_1D(ds1, ds2):
 	
 	return ds1, ds2
 
-def main():
-	forest_images_path = '/home/umar/dip_project/New_Try/Database/forest/*.jpg'
-	test_images_path = '/home/umar/dip_project/New_Try/Database/test/*.jpg'
-	images = load_images(forest_images_path)
+# BFMatcher implementation:
 
+def bf_matcher(des, n_des,k): # --> Useless
+	bf = cv2.BFMatcher()
+	matches = []
+	for i,a in enumerate(des):
+		match = bf.knnMatch(a,n_des,3)
+		li = [i,match]
+		matches.append(li)
+	print(matches)
+	return matches
+
+def main():
+	forest_images_path = '/home/umar/dip_project/new_try/Database/train/*.jpg'
+	test_images_path = '/home/umar/dip_project/new_try/Database/test/*.jpg'
+	images = load_images(forest_images_path)
 	images_test = load_images(test_images_path)
 
-	print("SIFT Working for training........")
-	kps, ds = featureExtraction(images)
-	print("SIFT Working for testin........")
-	kps_t, ds_t = featureExtraction(images_test)
-
-#	Classification
-
-	#t,te = reshape_1D(ds[0],ds_t[0]) # --> Reshaped correctly
+	try:
+		kps = keypoint_load(path_kp_train)
+		ds = pickle_load(path_ds_train)
+		kps_t = keypoint_load(path_kp_test)
+		ds_t = pickle_load(path_ds_test)
+		print('Load data successfully')
+	except Exception:
+		print('Generating keypoints and descriptors')
+		print("SIFT Working for training........")
+		ts = timer()
+		kps, ds = featureExtraction(images)
+		print('Length of keypoints')
+		print("SIFT Working for testing.........")
+		kps_t, ds_t = featureExtraction(images_test)
+		tf = timer()
+		(keypoint_save(kps,path_kp_train))
+		(pickle_save(ds,path_ds_train))
+		(keypoint_save(kps_t,path_kp_test))
+		(pickle_save(ds_t,path_ds_test))
+		print(f'Total time taking {tf-ts}')
+	
+#	Classification 
 	matches = knn(3, ds, ds_t[1])
 	print("Matches Images")
 	index = matches.keys()
-
-	pridicted_index = []
-
+	images_window = []
+	images_window.append(images_test[1])
 	for a in index:
-		pridicted_index.append(a)
+		images_window.append(images[a])
 
-	print(pridicted_index[0])
 	print("Original Image")
-	showImg(images_test[1],1)
+	#showImg(images_test[1],1)
+	disImage(images_window)
+	#disp_img = np.hstack((images_test[1], images[pridicted_index[0]],images[pridicted_index[1]],images[pridicted_index[1]]))
 	print("Pridicted Image")
-	showImg(images[pridicted_index[0]],1)
-	showImg(images[pridicted_index[1]],1)
-	showImg(images[pridicted_index[2]],1)
+	#cv2.imshow('Images', disp_img)
+	#cv2.waitKey()
+	#showImg(images[pridicted_index[2]],1)
 
 if __name__ == '__main__':
 	main()
